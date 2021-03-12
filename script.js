84a9c7bfd6e32f282c959035148cf405efd394a5
jmolloy501 <70666253+jmolloy501@users.noreply.github.com>
2021-03-12 13:13:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
-        var map = new Map({
-          basemap: "dark-gray"
-        });
+var map = L.map('map').setView([38.63, -90.29], 10);
 
-        var view = new MapView({
-          container: "viewDiv",
-          map: map,
-          zoom: 10,
-          center: [-89.85, 38.85] // longitude, latitude
-        });
-      });
+  // load a tile layer
+L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+}).addTo(map);
+
+  // load GeoJSON from an external file
+  $.getJSON("https://github.com/jmolloy501/GeoJSON/blob/main/ten_person_parcels_9am_Featu.geojson",function(data){
+    // add GeoJSON layer to the map once the file is loaded
+    L.geoJson(data).addTo(map);
+  });
+
+ $.getJSON("https://github.com/jmolloy501/GeoJSON/blob/main/9am_parcel_dots_8-1-19.geojson",function(data){
+    // add GeoJSON layer to the map once the file is loaded
+    L.geoJson(data).addTo(map);
+  });
